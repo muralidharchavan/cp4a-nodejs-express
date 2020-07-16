7d629be16eb2d45103ab5b1afa2846167b5a29db
maheshwarishikha <maheshwari.shikha@gmail.com>
2020-07-16 17:04:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from Appsody CP4A!");
+  res.send("Hello from Codewind CP4A!");
 });
  
 module.exports.app = app;

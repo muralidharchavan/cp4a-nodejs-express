8242a4d3d61c8a205502a9320cf1696696463b03
maheshwarishikha <maheshwari.shikha@gmail.com>
2020-07-17 17:13:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from Codewind CP4A!");
+  res.send("Hello from Codewind CP4A in different namespace!");
 });
  
 module.exports.app = app;

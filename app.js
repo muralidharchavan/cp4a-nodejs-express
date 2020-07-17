774e38e1f525598689e3c5bdad71e6ee015b435c
maheshwarishikha <maheshwari.shikha@gmail.com>
2020-07-17 18:10:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from Codewind CP4A deployed in weather-app project!");
+  res.send("Hello from Codewind CP4A pushed and deployed in weather-app namespace!");
 });
  
 module.exports.app = app;
